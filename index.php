3daf737358941f56da8b63ddd3360d4a106f7b14
Basvandesteeg <basvandesteeg@upcmail.nl>
2013-01-17 08:40:20 -0800

    Create index.php
    This is the main file you should include to your site.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+Including the autoLoad file to load al project files in one file.
+*/
+include_once("lib/autoLoad.php");
+/*
+Do not change to directory location of the autoLoad.php cause the programm could may not work.
+*/
+?>
